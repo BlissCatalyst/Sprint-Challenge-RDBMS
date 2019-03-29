660ff05305b104e38720d6fc78e5aabb9719cd6d
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-03-29 11:17:23 -0600

    added get endpoints.

--- index.js
+++ index.js
@@ -4,11 +4,31 @@ const knex = require('knex');
 const knexConfig = require('./knexfile.js');
 
 const db = knex(knexConfig.development);
+const Roles = require('./roles/role.js');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
+// ********** ENDPOINTS **********
+server.get('/api/projects', async (req, res) => {
+  try {
+    const all = await Roles.find();
+    res.status(200).json(all);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+server.get('/api/projects/:projectId', async (req, res) => {
+  try { 
+    const project = await Roles.findById(req.params.projectId);
+    res.status(200).json(project);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
 
 const port = process.env.PORT || 4444;
 server.listen(port, () => 
