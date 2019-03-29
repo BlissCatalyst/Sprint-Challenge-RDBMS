6b514291f57611a4195ea900925f547a8e90c08a
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-03-29 11:02:57 -0600

    added project and action tables to migration.

--- database/migrations/20190329103104_projects.js
+++ database/migrations/20190329103104_projects.js
@@ -1,8 +1,40 @@
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function(knex) {
+  return knex.schema
+  .createTable('projects', function(tbl) {
+    tbl.increments();
+
+    tbl
+      .string('name', 128)
+      .notNullable()
+      .unique();
+
+    tbl
+      .string('description', 128)
+      .notNullable();
+
+    tbl
+      .boolean('is_done')
+  })
+  .createTable('actions', tbl => {
+    tbl.increments();
+    
+    tbl
+      .string('description', 128)
+      .notNullable()
+      .unique();
+    tbl
+      .string('notes', 128);
+    tbl
+      .boolean('is_complete');
+    tbl
+      .integer('project_id')
+      .unsigned()
+      .references('id')
+      .inTable('projects');
+  })
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('projects');
 };
